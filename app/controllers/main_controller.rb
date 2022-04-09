fa97b440ce898c20568284d9b05c4c8edc56ceb3
Serge Kislak <kislak7@gmail.com>
2022-04-09 18:05:39 +0300

    counter

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,5 @@
+class MainController < ApplicationController
+  def increment
+    @counter = (params[:counter]&.to_i || 0) + 1
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'main#index'
+
+  get 'main/increment', as: :increment
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
